e117464be1bcb1d51b4cf2563c6103de0ed0ec72
yanira <yanira@DESKTOP-DQ6Q9TT>
2017-11-02 18:38:04 -0600

    prueba

--- BDProy2/src/bdproy2/BDProy2.java
+++ BDProy2/src/bdproy2/BDProy2.java
@@ -16,6 +16,7 @@ public class BDProy2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // Esto solo es una prueba hecha por Arturo 
     }
     
 }
